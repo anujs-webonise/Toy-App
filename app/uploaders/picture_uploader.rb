486c99da4983374fab28b2767521680ca57dd02e
anujs-webonise <anuj.shah@weboniselab.com>
2018-06-07 12:40:52 +0530

    Authour name,email,max upload size managed dynamically - File size managed from client side - Email_from change in user_test file

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -44,6 +44,10 @@ class PictureUploader < CarrierWave::Uploader::Base
     %w(jpg jpeg gif png)
   end
 
+  def size_range
+    1..MAX_IMAGE_UPLOAD_SIZE.megabytes
+  end
+  
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
   # def filename
--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,1 +1,8 @@
-APP_NAME = 'Mini Twitter App'
+APP_NAME = 'Mini Twitter App'.freeze
+AUTHOUR_NAME = 'Anuj Shah'.freeze
+AUTHOUR_EMAIl = 'anuj.shah@weboniselab.com'.freeze
+COMPANY_NAME = 'Webonise Labs Pvt. Ltd.'.freeze
+COMPANY_WEBSITE = 'https://www.webonise.com'.freeze
+EMAIL_FROM = 'noreply@mini-twitter-app-rails.com'.freeze
+MAX_IMAGE_UPLOAD_SIZE = 5 # In MB
+
